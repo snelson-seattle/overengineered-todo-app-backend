794f6efb4167ff8a10ff4d3cbd0efbde8f54d161
Scott Nelson <snelson@instructors.2u.com>
2022-11-06 11:27:21 -0800

    added the scaffolding for an authentication system to the backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [UserModule, TaskModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
